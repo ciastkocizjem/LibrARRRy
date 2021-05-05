606c733c9dea190555cd60d3d2a6db656bf00b1d
Basia Białostocka <baska.b0@op.pl>
2021-05-05 15:01:56 +0200

    Filtering working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,24 +30,53 @@ namespace LibrARRRy.Controllers
         }
 
         [HttpPost]
-        public ActionResult Filter(FormCollection formCollection)
+        public ActionResult Index(FormCollection formCollection)
         {
             var books = db.Books;
             var categories = db.Categories.OrderBy(c => c.Name);
             var tags = db.Tags.OrderBy(t => t.Name);
 
-            List<string> selecteds = new List<string>();
+            List<string> selectedCategories = new List<string>();
+            List<string> selectedTags = new List<string>();
 
             foreach (var item in formCollection.AllKeys)
             {
                 if ((bool)this.ValueProvider.GetValue(item).ConvertTo(typeof(bool)))
                 {
-                    selecteds.Add(item);
+                    if (item.StartsWith("C_"))
+                    {
+                        selectedCategories.Add(item.Replace("C_", ""));
+                    }
+                    else
+                    {
+                        if (item.StartsWith("T_"))
+                        {
+                            selectedTags.Add(item.Replace("T_", ""));
+                        }
+                    }
                 }
-
             }
 
-            
+            List<Book> selectedBooks = new List<Book>();
+
+            foreach (Book book in books)
+            {
+                if (selectedCategories.Contains(book.Category.Name))
+                {
+                    selectedBooks.Add(book);
+                    continue;
+                }
+
+                if (selectedTags.Any(t => book.Tags.Any(bt => bt.Name == t)))
+                {
+                    selectedBooks.Add(book);
+                    continue;
+                }
+            }
+
+            ViewBag.BooksList = selectedBooks;
+            ViewBag.CategoriesList = categories.ToList();
+            ViewBag.TagsList = tags.ToList();
 
             return View();
         }
