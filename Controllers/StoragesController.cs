fb0aba09b591b4e235971a4ca4e75b0f3fe40783
ans99 <ans99@onet.pl>
2021-05-16 14:02:28 +0200

    Added storage controller

---
+++ Controllers/StoragesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LibrARRRy.DAL;
+using LibrARRRy.Models;
+
+namespace LibrARRRy.Controllers
+{
+    //TODO: CURRENT AMOUNT = AMOUNT IN CREATE
+    public class StoragesController : Controller
+    {
+        private LibrARRRyContext db = new LibrARRRyContext();
+
+        // GET: Storages
+        public ActionResult Index()
+        {
+            return View(db.Storages.ToList());
+        }
+
+        // GET: Storages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Storage storage = db.Storages.Find(id);
+            if (storage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(storage);
+        }
+
+        // GET: Storages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Storages/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BookId,Amount,CurrentAmount")] Storage storage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Storages.Add(storage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(storage);
+        }
+
+        // GET: Storages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Storage storage = db.Storages.Find(id);
+            if (storage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(storage);
+        }
+
+        // POST: Storages/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BookId,Amount,CurrentAmount")] Storage storage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(storage).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(storage);
+        }
+
+        // GET: Storages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Storage storage = db.Storages.Find(id);
+            if (storage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(storage);
+        }
+
+        // POST: Storages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Storage storage = db.Storages.Find(id);
+            db.Storages.Remove(storage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
