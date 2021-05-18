56c49f952b6d50d8ffea057aff600ecfe06fbd0e
ans99 <ans99@onet.pl>
2021-05-18 12:41:04 +0200

    Added controller for shopping cart using session

---
+++ Controllers/ShoppingCartController.cs
@@ -0,0 +1,52 @@
+﻿using LibrARRRy.DAL;
+using LibrARRRy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibrARRRy.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private LibrARRRyContext db = new LibrARRRyContext();
+
+        // GET: ShopiingCart
+        public ActionResult Add(Book book)
+        {
+            if (Session["cart"] == null)
+            {
+                List<Book> books = new List<Book>();
+                books.Add(book);
+                ViewBag.cart = books.Count();
+
+                Session["count"] = 1;
+            } 
+            else
+            {
+                List<Book> books = (List<Book>)Session["cart"];
+                books.Add(book);
+                Session["cart"] = books;
+                ViewBag.cart = books.Count();
+                Session["count"] = Convert.ToInt32(Session["count"]) + 1;
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Order()
+        {
+            return View((List<Book>)Session["cart"]);
+        }
+
+        public ActionResult Remove(Book book)
+        {
+            List<Book> books = (List<Book>)Session["cart"];
+            books.RemoveAll(b => b.BookId == book.BookId);
+            Session["cart"] = books;
+            Session["count"] = Convert.ToInt32(Session["count"]) - 1;
+
+            return RedirectToAction("Order", "ShoppingCart");
+        }
+    }
+}
