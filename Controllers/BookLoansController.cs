66848468aa6b3d600258f12bc99ac6c4e703e955
ans99 <ans99@onet.pl>
2021-05-18 21:04:35 +0200

    Prepared book loans

---
+++ Controllers/BookLoansController.cs
@@ -0,0 +1,27 @@
+﻿using LibrARRRy.DAL;
+using LibrARRRy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibrARRRy.Controllers
+{
+    public class BookLoansController : Controller
+    {
+        private readonly LibrARRRyContext db = new LibrARRRyContext();
+
+        // GET: BookLoans
+        public ActionResult Loans()
+        {
+            List<Book> books = db.Books.ToList();
+            return View(books);
+        }
+
+        public ActionResult Return(Book book)
+        {
+            return RedirectToAction("Loans", "BookLoans");
+        }
+    }
+}
