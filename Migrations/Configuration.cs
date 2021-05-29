578c8886ef76c453bc0956d5ec169ab0d9eb5901
cholibka <51329853+cholibka@users.noreply.github.com>
2021-05-29 18:07:06 +0200

    mERge <3 (#28)
    * Update merge (#22)
    
    * Sending reminder mails working (#14)
    
    * Update merge (#13)
    
    * Wjechałam Ci na brancha (Update from master)
    
    * Loaning book working.
    
    * Added authorization to loans
    
    Co-authored-by: cholibka <51329853+cholibka@users.noreply.github.com>
    
    * Fixed loaning adding returning books (#8)
    
    * Loaning book working.
    
    * Added authorization to loans
    
    * Created loans (#5) (#7)
    
    * Loaning book working.
    
    * Added authorization to loans
    
    * Added returning books (incrementing storage amount and adding date of returning)
    
    * Changed location of manage panel and added delete for reader
    
    * Removed redundancy
    
    * Added user manage and changing role
    
    * Added loans to manage panel
    
    * Minor changes in manage panel
    
    * Sending emails for reseting password works :)
    
    Co-authored-by: cholibka <51329853+cholibka@users.noreply.github.com>
    Co-authored-by: ans99 <ans99@onet.pl>
    
    * Added collection date to loan model, regenerated views and controller
    
    * Started work on sening delayed emails
    
    * Sending reminder mails working
    
    Co-authored-by: cholibka <51329853+cholibka@users.noreply.github.com>
    Co-authored-by: ans99 <ans99@onet.pl>
    
    * reader archive done <3 added validation for edit book
    
    * started working on admin massages in home page
    
    * button for edit is visible only for admin
    
    * Reading from file works
    
    * edit message in view works
    
    * ajax comes all in white and saves everything ily
    
    * i love you i hate you i hate that i want you ajax
    
    * Search and filtering using AJAX/JQuerry (#21)
    
    * added hidding newest books :)
    
    * ajax how i hate u but i love u
    
    Co-authored-by: cholibka <51329853+cholibka@users.noreply.github.com>
    Co-authored-by: ans99 <ans99@onet.pl>
    
    Co-authored-by: cholibka <51329853+cholibka@users.noreply.github.com>
    Co-authored-by: ans99 <ans99@onet.pl>
    
    * Added application settings file
    
    BooksLimit - amount of books
    DetentionLimit - enum-like var
    AdminMessage
    
    * Added scheduling job for applying penalties
    
    * Implemented adding searches, button to save search almost working
    
    * started working on index making project pretty
    
    * SaveSearch doesn't display weird view, saving search fully functional
    
    * Changed search index and delete
    
    * Added creating roles, users and first book to seed method
    
    * Added if to not add search with empty book list
    
    Co-authored-by: Barbara Białostocka <51329818+ciastkocizjem@users.noreply.github.com>
    Co-authored-by: Basia Białostocka <baska.b0@op.pl>

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -1,10 +1,15 @@
 ﻿namespace LibrARRRy.Migrations
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Diagnostics;
     using System.Linq;
     using LibrARRRy.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     internal sealed class Configuration : DbMigrationsConfiguration<LibrARRRy.DAL.LibrARRRyContext>
     {
@@ -15,37 +20,65 @@
 
         protected override void Seed(LibrARRRy.DAL.LibrARRRyContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            // Creating roles, users and assigning roles to users
+            string[] roles = { "admin", "worker", "reader" };
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            foreach (string role in roles)
+            {
+                // Creating roles
+                if (!context.Roles.Any(r => r.Name == role))
+                {
+                    context.Roles.Add(new IdentityRole(role));
+                }
+                // Creating user for role
+                string email = $"{role}@{role}.com";
+                if (!context.Users.Any(u => u.Email == email))
+                {
+                    string password = role.First().ToString().ToUpper() + role.Substring(1);
+                    ApplicationUser user = new ApplicationUser
+                    {
+                        Email = email,
+                        PasswordHash = userManager.PasswordHasher.HashPassword($"{password}123."),
+                        EmailConfirmed = true,
+                        SecurityStamp = Guid.NewGuid().ToString("D"),
+                        UserName = email,
+                    };
+                    userManager.Create(user);
+                    // Add user to role
+                    userManager.AddToRole(user.Id, role);
+                }
+            }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            // Creating first book and components needed
+            Author author = new Author { Name = "Dmitry", Surname = "Glukhovsky" };
+            Tag tag = new Tag { Name = "Przyszłość" };
+            Category category = new Category { Name = "Fantastyka" };
 
-            //context.Categories.AddOrUpdate(new Models.Category { CategoryId = 1, Name = "Sci-Fi" });
-            //context.Authors.AddOrUpdate(new Models.Author { AuthorId = 1, Name = "Dmitrrrry", Surname = "Glukoza" });
-            //context.Books.AddOrUpdate(new Models.Book
-            //{
-            //    Title = "Przyszłość",
-            //    AdditionDate = DateTime.Now,
-            //    BookId = 1,
-            //    ISBN = "9900OP",
-            //    Category = context.Categories.First(),
-            //    CategoryId = context.Categories.First().CategoryId,
-            //    Authors = context.Authors.First().
-            //});
+            Book book = new Book { Title = "Futu.re", Description = "Pokonaliśmy śmierć. I co dalej?", ISBN = "14842061", AdditionDate = DateTime.Now, CategoryId = category.CategoryId };
+            book.Authors = new List<Author>();
+            book.Authors.Add(author);
+            book.Tags = new List<Tag>();
+            book.Tags.Add(tag);
 
-            //IdentityManager im = new IdentityManager();
-            //// Create admin
-            //im.CreateUser(new ApplicationUser() { Email = "admin@librarrry.com", UserName = "Admin" }, "Admin123.");
-            //im.CreateUser(new ApplicationUser() { Email = "worker@librarrry.com", UserName = "Worker" }, "Worker123.");
-            //im.CreateUser(new ApplicationUser() { Email = "anita@mail.com", UserName = "Anita" }, "Anita123.");
-            //// Create roles
-            //im.CreateRole("admin");
-            //im.CreateRole("worker");
-            //im.CreateRole("reader");
-            //// Assign roles
-            //im.AddUserToRoleByUsername("admin@librarrry.com", "admin");
-            //im.AddUserToRoleByUsername("worker@librarrry.com", "worker");
-            //im.AddUserToRoleByUsername("anita@mail.com", "reader");
+            if (!context.Authors.Any(a => a.Surname == author.Surname && a.Name == author.Name))
+            {
+                context.Authors.Add(author);
+            }
+            if (!context.Tags.Any(t => t.Name == tag.Name))
+            {
+                context.Tags.Add(tag);
+            }
+            if (!context.Categories.Any(c => c.Name == category.Name))
+            {
+                context.Categories.Add(category);
+            }
+
+            if (!context.Books.Any(b => b.Title == book.Title && b.ISBN == book.ISBN))
+            {
+                context.Books.Add(book);
+            }
+
+            context.SaveChanges();
         }
     }
 }
